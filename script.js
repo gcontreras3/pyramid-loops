0bf6cb096e41aba44074b270c1a36a5e0e936af9
Jerry <ivan@Jerrys-Laptop.local>
2025-01-31 10:38:02 -0600

    made changes to loops

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let inverted = false;
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
+// TODO: use a different type of loop
 for (let i = 1; i <= count; i++) {
     if (inverted) {
       rows.unshift(padRow(i, count));
@@ -14,13 +15,14 @@ for (let i = 1; i <= count; i++) {
     }
   }
   
-  while (rows.length < count) {
+  /* while (rows.length < count) {
     rows.push(padRow(rows.length + 1, count));
-  }
-  
-  for (let i = count; i > 0; i--) {
+  }*/
+
+ /* for (let i = count; i > 0; i--) {
     rows.push(padRow(i, count));
-  }
+  } */
+
 let result = ""
 
 for (const row of rows) {
